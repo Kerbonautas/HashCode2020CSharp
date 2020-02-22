5f494f1b136ab02b8a19e04519a30479626af157
Juan Antonio Lagos Carrera <lagoscarrera@gmail.com>
2020-02-22 22:16:00 +0100

    Books are sorted now by its value inside each library

---
+++ Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashcode2020CSharp
+{
+    class Book
+    {
+        public int id { get; set; }
+        public int value { get; set; }
+        public Book(int id, int value)
+        {
+            this.id = id;
+            this.value = value;
+        }
+    }
+}
